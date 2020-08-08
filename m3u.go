daae71668058a4bc076d10c6ed4bd7f7c2a8be3c
Dale Campbell <oshuma@gmail.com>
2020-08-08 00:07:06 -0400

    Adding json tags to Track. Noisy commit, since it fixes formatting also.

--- m3u.go
+++ m3u.go
@@ -27,11 +27,11 @@
 package m3u
 
 import (
-  "bufio"
-  "fmt"
-  "io"
-  "strconv"
-  "strings"
+	"bufio"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
 )
 
 // Represents a list of tracks.
@@ -39,89 +39,89 @@ type Playlist []Track
 
 // Represents a single track.
 type Track struct {
-  Path  string // path to the file
-  Title string // title of the track
-  Time  int64  // duration of the track
+	Path  string `json:"path"`  // path to the file
+	Title string `json:"title"` // title of the track
+	Time  int64  `json:"time"`  // duration of the track
 }
 
 // Parses simple and extended m3u files. Returns the playlist.
 func Parse(r io.Reader) (Playlist, error) {
-  br := bufio.NewReader(r)
-  pl := Playlist{}
+	br := bufio.NewReader(r)
+	pl := Playlist{}
 
-  for {
-    line, err := br.ReadString('\n')
+	for {
+		line, err := br.ReadString('\n')
 
-    if err != nil {
-      if err == io.EOF {
-        return pl, nil
-      }
-      return pl, err
-    }
-    line = line[:len(line)-1]
+		if err != nil {
+			if err == io.EOF {
+				return pl, nil
+			}
+			return pl, err
+		}
+		line = line[:len(line)-1]
 
-    if len(line) > 0 && line[0] != '#' {
-      pl = append(pl, Track{Path: line, Title: "", Time: -1})
-      continue
-    }
+		if len(line) > 0 && line[0] != '#' {
+			pl = append(pl, Track{Path: line, Title: "", Time: -1})
+			continue
+		}
 
-    if len(line) > 8 && line[:8] == "#EXTINF:" {
-      i := strings.Index(line[8:], ",")
+		if len(line) > 8 && line[:8] == "#EXTINF:" {
+			i := strings.Index(line[8:], ",")
 
-      if i < 0 {
-        return pl, fmt.Errorf("unexpected line: %q", line)
-      }
-      ftime, err := strconv.ParseFloat(line[8:i+8], 64)
+			if i < 0 {
+				return pl, fmt.Errorf("unexpected line: %q", line)
+			}
+			ftime, err := strconv.ParseFloat(line[8:i+8], 64)
 
-      if err != nil {
-        return pl, err
-      }
-      time := int64(ftime)
-      path, err := br.ReadString('\n')
+			if err != nil {
+				return pl, err
+			}
+			time := int64(ftime)
+			path, err := br.ReadString('\n')
 
-      if err != nil {
-        return pl, err
-      }
-      pl = append(pl, Track{Path: path[:len(path)-1], Title: line[i+9:], Time: time})
-    }
-  }
+			if err != nil {
+				return pl, err
+			}
+			pl = append(pl, Track{Path: path[:len(path)-1], Title: line[i+9:], Time: time})
+		}
+	}
 }
 
 // Writes the playlist to a writer in the extended m3u format. Returns the
 // number of written bytes.
 func (pl Playlist) WriteTo(w io.Writer) (n int, err error) {
-  if n, err = fmt.Fprintln(w, "#EXTM3U"); err != nil {
-    return
-  }
+	if n, err = fmt.Fprintln(w, "#EXTM3U"); err != nil {
+		return
+	}
 
-  for _, t := range pl {
-    time := t.Time
+	for _, t := range pl {
+		time := t.Time
 
-    if time < 1 {
-      time = -1
-    }
-    m, err := fmt.Fprintf(w, "#EXTINF:%d,%s\n%s\n", time, t.Title, t.Path)
+		if time < 1 {
+			time = -1
+		}
+		m, err := fmt.Fprintf(w, "#EXTINF:%d,%s\n%s\n", time, t.Title, t.Path)
 
-    if err != nil {
-      return n, err
-    }
-    n += m
-  }
-  return
+		if err != nil {
+			return n, err
+		}
+		n += m
+	}
+	return
 }
 
 // Writes the playlist to a writer in the simple m3u format. Returns the number
 // of written bytes.
 func (pl Playlist) WriteSimpleTo(w io.Writer) (n int, err error) {
-  n = 0
+	n = 0
 
-  for _, t := range pl {
-    m, err := fmt.Fprintln(w, t.Path)
+	for _, t := range pl {
+		m, err := fmt.Fprintln(w, t.Path)
 
-    if err != nil {
-      return n, err
-    }
-    n += m
-  }
-  return
+		if err != nil {
+			return n, err
+		}
+		n += m
+	}
+	return
 }
